20acd27c3af954e2f1005ee9c9a3c48cdba0d74e
ravali <ravali@ravali-macbook.attlocal.net>
2018-09-24 22:52:31 -0500

    logistic regression

--- src/logisticregression.py
+++ src/logisticregression.py
@@ -1,25 +1,42 @@
-DIR='/Users/ravali/Desktop/DataScience-Classification/data'
+DIR='/Users/ravali/Desktop'
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-import matplotlib.pyplot as plt
-import seaborn as sns 
-predictors = pd.read_csv(DIR+'/train_predictors.csv', delimiter=',')
-test_predictors = pd.read_csv(DIR+'/test_predictors.csv', delimiter=',')
-targets = pd.read_csv(DIR+'/train_targets.csv', delimiter=',')
-predictors = predictors[pd.to_numeric(predictors.ActSceneLine, errors='coerce').notnull()]
-predictors.ActSceneLine = predictors.ActSceneLine.astype(float)
-print(predictors.dtypes)
-predictors = predictors[pd.to_numeric(predictors.Dataline, errors='coerce').notnull()]
-predictors.Dataline = predictors.ActSceneLine.astype(float)
-predictors = predictors[pd.to_numeric(predictors.Play, errors='coerce').notnull()]
-predictors.Play = predictors.Play.astype(float)
-predictors = predictors[pd.to_numeric(predictors.PlayerLine, errors='coerce').notnull()]
-predictors.PlayerLine = predictors.PlayerLine.astype(float)
-print(predictors.describe())
-sns.FacetGrid(predictors, hue="Player").map(plt.scatter, "ActSceneLine", "PlayerLine").add_legend()
-lr=LogisticRegression(class_weight= None)
-lr.fit(predictors, targets)
-test_out = lr.predict(test_predictors)
-print (type(test_out)) 
-test_out.to_csv('/Users/ravali/Desktop/DataScience-Classification/data/output.csv', sep=',') 
+df1 = pd.read_csv(DIR+'/sp1.csv', delimiter=',')
+#print (df1)
+#print(df1.head())
+#print(df1.dtypes) 
+df1=df1.dropna(how='any')
+obj_df = df1.select_dtypes(include=['object']).copy()
+obj_df.head()
+obj_df["Play"] = obj_df["Play"].astype('category')
+obj_df["Play"] = obj_df["Play"].cat.codes
+obj_df["ActSceneLine"] = obj_df["ActSceneLine"].astype('category')
+obj_df["ActSceneLine"] = obj_df["ActSceneLine"].cat.codes
+obj_df["Player"] = obj_df["Player"].astype('category')
+obj_df["Player"] = obj_df["Player"].cat.codes
+obj_df["PlayerLine"] = obj_df["PlayerLine"].astype('category')
+obj_df["PlayerLine"] = obj_df["PlayerLine"].cat.codes
+#print(obj_df.head())
+df1 = pd.concat([df1, obj_df], axis=1)
+df1=df1.drop('Play',1)
+df1=df1.drop('ActSceneLine',1)
+df1=df1.drop('PlayerLine',1)
+df1=df1.drop('Player',1)
+df1 = pd.concat([df1, obj_df], axis=1)
+#print(df1['Play'])
+Y=df1.Player #output label
+X=df1.drop('Player',axis=1)#set without outputlabel testset
+#print(X)
+X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size=0.2)
+#print ("\nX_train:\n")
+#print(X_train.head())
+#print (X_train.shape)
+#print ("\nX_test:\n")
+#print(X_test.head())
+#print (X_test.shape)
+logisticRegr = LogisticRegression()
+logisticRegr.fit(X_train, y_train)
+redictions = logisticRegr.predict(X_test)
+print(score = logisticRegr.score(X_test, y_test))
